e212a4f30379f3f2fd0025e6b35996e709746608
Rammsat <saturn9911@mail.ru>
2023-01-12 19:41:17 +0300

    Добавлен алюр и атачи

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,63 @@
 import pytest
+import os
 from selene.support.shared import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selene import Browser, Config
+from dotenv import load_dotenv
+from demoqa_tests.utils import attach
+
+
+DEFAULT_BROWSER_VERSION = "100.0"
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version',
+        default='100.0'
+    )
+
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
 
 
 @pytest.fixture(scope="function", autouse=True)
-def open_browser():
+def open_browser(request):
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = DEFAULT_BROWSER_VERSION
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": browser_version,
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+
+    options.capabilities.update(selenoid_capabilities)
+    """
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+
+    driver = webdriver.Remote(
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser_for_attach = Browser(Config(driver))
+"""
     browser.config.window_width = 1400
     browser.config.window_height = 800
     browser.config.base_url = 'https://demoqa.com'
 
     yield
+
+    attach.add_html(browser)
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    #attach.add_video(browser_for_attach)
 
     browser.quit()
--- tests/practice_form_test.py
+++ tests/practice_form_test.py
@@ -1,40 +1,43 @@
 from demoqa_tests.model.pages import practice_form
+import allure
 
 
 def test_student_registration_form():
-    practice_form.given_opened()
+    with allure.step("Open registrations form"):
+        practice_form.given_opened()
 
     # WHEN
-    practice_form.type_firstname('World')
-    practice_form.type_lastname('Peace')
-    practice_form.type_email('qwe@mail.com')
+    with allure.step("Fill form"):
+        practice_form.type_firstname('World')
+        practice_form.type_lastname('Peace')
+        practice_form.type_email('qwe@mail.com')
 
-    practice_form.select_gender('Male')
+        practice_form.select_gender('Male')
 
-    practice_form.type_phone_number('9998887755')
+        practice_form.type_phone_number('9998887755')
 
-    practice_form.click_on_datepicker()
-    practice_form.pick_month('May')
-    practice_form.pick_year('1999')
-    practice_form.pick_day(11)
+        practice_form.click_on_datepicker()
+        practice_form.pick_month('May')
+        practice_form.pick_year('1999')
+        practice_form.pick_day(11)
 
-    practice_form.type_subject('English')
+        practice_form.type_subject('English')
 
-    practice_form.select_hobby('Sports')
+        practice_form.select_hobby('Sports')
 
-    practice_form.scroll_to_address()
-    practice_form.type_address('Some address')
+        practice_form.scroll_to_address()
+        practice_form.type_address('Some address')
 
-    practice_form.upload_picture('resources/image.PNG')
+        practice_form.upload_picture('resources/image.PNG')
 
-    practice_form.select_state('NCR')
-    practice_form.select_city('Delhi')
+        practice_form.select_state('NCR')
+        practice_form.select_city('Delhi')
 
-    practice_form.submit()
+        practice_form.submit()
 
     # THEN
-
-    practice_form.assert_fields(
+    with allure.step("Check form results"):
+        practice_form.assert_fields(
             'World Peace',
             'qwe@mail.com',
             'Male',
